fe0220327a44145fa76b64097d56b4df869f0c8c
Welschoff <h.welschoff@gmx.de>
2022-03-11 09:06:12 +0100

    task6 revised

--- aufgabe6.java
+++ aufgabe6.java
@@ -6,7 +6,8 @@ public class aufgabe6 {
         boolean wahrheit2;
         boolean wahrheit3;
         boolean wahrheit4;
- 
+        boolean result;
+
         boolean wert1 = true;
         boolean wert2 = false;
  
@@ -14,10 +15,12 @@ public class aufgabe6 {
         wahrheit2 = wert1 && !wert2;
         wahrheit3 = !wert1 && !wert2;
         wahrheit4 = !wert1 && wert2;
+        result = wahrheit1 || wahrheit2 || wahrheit3 || wahrheit4;
  
-         System.out.println(wahrheit1);
-         System.out.println(wahrheit2);
-         System.out.println(wahrheit3);
-         System.out.println(wahrheit4);
+         System.out.println("wahrheit1 = " + wahrheit1);
+         System.out.println("wahrheit2 = " + wahrheit2);
+         System.out.println("wahrheit3 = " + wahrheit3);
+         System.out.println("wahrheit4 = " + wahrheit4);
+         System.out.println("result = " + result);
      }
 }
