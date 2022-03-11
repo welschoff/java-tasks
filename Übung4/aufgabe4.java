f034239278e4b4d755615862e1e5cedd7c8a9999
Welschoff <h.welschoff@gmx.de>
2022-03-11 16:01:40 +0100

    aufgabe4

---
+++ Übung4/aufgabe4.java
@@ -0,0 +1,16 @@
+package Übung4;
+
+public class aufgabe4 {
+
+public static void main(String[] args) {
+
+    int zahl = 123;
+    int summe = 0;
+
+    while (zahl > 0) {
+        summe += zahl % 10;
+        zahl /= 10;
+    }
+    System.out.println(summe);
+}    
+}
